179ca3065ab8c28128fa12d981ffa4896001f618
RochaThomas <tomrocha99@gmail.com>
2022-09-29 16:32:25 -0700

    finished converting to nearby search

--- server.py
+++ server.py
@@ -5,34 +5,7 @@ app.secret_key = "Secret secrets are no fun secret secrets hurt someone"
 
 @app.route("/")
 def testing_something():
-    
-    # in theory... axios call here to get data for places
-    # then send places as a list to html page as a variable representing a list
-    # use jinja code in the html file to display the respective information
-
-    places = [
-        {
-            "name": "Fake Address",
-            "address": "1234 Seasame Street, San Francisco, California 94016",
-            "coords": { "lat": 37.3387, "lng": -121.8853 },
-            "content": '<h1>Testing Testing</h1>',
-            
-        },
-        {
-            "name": "Another Fake Place",
-            "address": "4321 Main Street, Sacramento, California 94203",
-            "coords": { "lat": 37.7749, "lng": -122.4194 },
-            "content": '<h1>Dirty City</h1>',
-        },
-        {
-            "name": "Fake Las Vegas Place",
-            "address": "5678 Sahara Lane, Las Vegas, Nevada 89139",
-            "coords": { "lat": 36.9741, "lng": -122.0308 },
-            "content": '<h1>Beach</h1>',
-        },
-    ];
-
-    return render_template("index_testing.html", places = places)
+    return render_template("index_testing.html")
 
 @app.route("/process_form", methods=['POST'])
 def process_form():
@@ -42,12 +15,13 @@ def process_form():
 @app.route("/form_results")
 def disp_form_results():
     form = session['form']
+    print(form)
 
-    addresses = []
+    places = []
     for key in form:
-        addresses.append(form[key])
+        places.append(form[key])
         
-    return render_template("testing_form_results.html", addresses=addresses)
+    return render_template("testing_form_results.html", places=places)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -2,223 +2,6 @@
 // javascript functions that will run the map
 // google maps api key AIzaSyApYkTOlVGaCHsdFi6DFG3JwH4y272RH1M
 
-// places = [
-//     {
-//         name: "Fake Address",
-//         address: "1234 Seasame Street, San Francisco, California 94016",
-//         coords: { lat: 37.3387, lng: -121.8853 },
-//         content: '<h1>Testing Testing</h1>',
-        
-//     },
-//     {
-//         name: "Another Fake Place",
-//         address: "4321 Main Street, Sacramento, California 94203",
-//         coords: { lat: 37.7749, lng: -122.4194 },
-//         content: '<h1>Dirty City</h1>',
-//     },
-//     {
-//         name: "Fake Las Vegas Place",
-//         address: "5678 Sahara Lane, Las Vegas, Nevada 89139",
-//         coords: { lat: 36.9741, lng: -122.0308 },
-//         content: '<h1>Beach</h1>',
-//     },
-// ];
-
-// var markers = []
-
-// function initMap() {
-    
-//     const icon = {
-//         url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-//         scaledSize: new google.maps.Size(25, 25),
-//         anchor: new google.maps.Point(0,25),
-//     }
-
-//     /** 
-//     * This location should be centered on the users input location
-//     * home, work, friend's house, etc.
-//     * right now it is centered on San Jose for demonstration purposes\
-//     * the users location would be input by them and would come from a call
-//     * to the database to retrieve users information or 
-//     * maybe it would come from the html page as a hidden input...
-//     * maybe a security issue doing it the second way 
-//     */
-//     const defaultLocation = { lat: 37.3387, lng: -121.8853 };
-
-//     // center the map on the default location
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 8,
-//         center: defaultLocation,
-//     });
-
-//     // add marker function
-//     let prevWindow = false;
-
-//     const addMarker = (place) => {
-//         const marker = new google.maps.Marker({
-//             position: place.coords,
-//             map: map,
-//             address : place.address,
-//             icon: icon,
-//             added: false,
-//         })
-        
-//         const infoWindow = new google.maps.InfoWindow({
-//             content: ""
-//         })
-
-//         infoWindow.setContent(place.content);
-        
-//         marker.addListener("mouseover", function(){
-//             if (prevWindow){
-//                 prevWindow.close()
-//             }
-
-//             prevWindow = infoWindow;
-//             infoWindow.open(map, marker);
-//         });
-//         markers.push(marker)
-//     }
-
-
-//     for (let i = 0; i < places.length; i++){
-//         addMarker(places[i]);
-//     }
-// }
-
-// window.initMap = initMap;
-
-// // testing reaction and information passing
-// /**
-//  * might run into a small problem here
-//  * when you have the icon reacting to the addToList Function
-//  * and then the user comes back to hover over the list it might change the marker
-//  * icon back to the flag...
-//  * maybe avoid this by having a boolean value in marker such as "added"
-//  * and then have an if statement to check if added is true or false
-//  * based on the bool value alter the object in markers[i].setIcon()
-//  */
-
-// // not sure why but it doesn't like these variables
-// // "google is not defined"
-
-// // let unadded = {
-// //     url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-// //     scaledSize: new google.maps.Size(25, 25),
-// //     anchor: new google.maps.Point(0,25),
-// // }
-
-// // let unaddedBig = {
-// //     url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-// //     scaledSize: new google.maps.Size(35, 35),
-// //     anchor: new google.maps.Point(0,35),
-// // }
-
-// // let added = {
-// //     url: "/static/checkPin.png",
-// //     scaledSize: new google.maps.Size(25, 25),
-// //     anchor: new google.maps.Point(0,25),
-// // }
-
-// // let addedBig = {
-// //     url: "/static/checkPin.png",
-// //     scaledSize: new google.maps.Size(35, 35),
-// //     anchor: new google.maps.Point(0,35),
-// // }
-
-// const iconReaction = (address) => {
-//     for (let i = 0; i < markers.length; i++) {
-//         if (markers[i].address == address) {
-//             if (markers[i].added){
-//                 markers[i].setIcon({
-//                     // addedBig
-//                     url: "/static/checkPin.png",
-//                     scaledSize: new google.maps.Size(35, 35),
-//                     anchor: new google.maps.Point(0,35),
-//                 })
-//             }
-//             else {
-//                 markers[i].setIcon({
-//                     // unaddedBig
-//                     url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-//                     scaledSize: new google.maps.Size(35, 35),
-//                     anchor: new google.maps.Point(0,35),
-//                 })
-//             }
-//         }
-//     }
-// }
-
-// const iconUndoReaction = (address) => {
-//     for (let i = 0; i < markers.length; i++) {
-//         if (markers[i].address == address) {
-//             if (markers[i].added){
-//                 markers[i].setIcon({
-//                     // added
-//                     url: "/static/checkPin.png",
-//                     scaledSize: new google.maps.Size(25, 25),
-//                     anchor: new google.maps.Point(0,25),
-//                 })
-//             }
-//             else {
-//                 markers[i].setIcon({
-//                     // unadded
-//                     url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-//                     scaledSize: new google.maps.Size(25, 25),
-//                     anchor: new google.maps.Point(0,25),
-//                 })
-//             }
-//         }
-//     }
-// }
-
-
-// /**
-//  * this function will alter a markers "added" boolean attribute
-//  * to true or false so the icons can react properly
-//  * but on the html side
-//  * all of the restaurants will have checks...
-//  * or some sort of input handler so that the list can
-//  * be sent to the back end
-//  */
-// const addToFavs = (address) => {
-//     for (let i = 0; i < markers.length; i++) {
-//         if (markers[i].address == address) {
-//             if (markers[i].added){
-//                 markers[i].added = false
-//                 markers[i].setIcon({
-//                     // unaddedBig
-//                     url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-//                     scaledSize: new google.maps.Size(35, 35),
-//                     anchor: new google.maps.Point(0,35),
-//                 })
-//             }
-//             else {
-//                 markers[i].added = true
-//                 markers[i].setIcon({
-//                     // addedBig
-//                     url: "/static/checkPin.png",
-//                     scaledSize: new google.maps.Size(35, 35),
-//                     anchor: new google.maps.Point(0,35),
-//                 })
-//             }
-//         }
-//     }
-// }
-
-// find a way to see the data from the form
-// this kind of works...
-// you can see all the worked out html but not the submission
-// we need to see the submission to see how we will access it
-// for posting to a database
-// for loop or access by id
-// if you access by id will it even work?
-// how would you know what is clicked and what is not clicked
-// const showMeTheData = (listForm) => {
-//     var data = document.getElementById(listForm);
-//     console.log(data.outerHTML);
-// }
-
 // try number two
 //  ---------------------
 
@@ -254,13 +37,11 @@ function initMap(){
 
         // check if the response from the request is okay
         if (status == google.maps.places.PlacesServiceStatus.OK) {
-            console.log("pass status check");
-            // places = google.maps.places.PlaceResult;
             places = results;
 
             for (const place of places) {
                 if (place.geometry && place.geometry.location){
-                    marker = new google.maps.Marker(
+                    const marker = new google.maps.Marker(
                         {
                             position: place.geometry.location,
                             map: map,
@@ -276,7 +57,7 @@ function initMap(){
 
                         prevWindow = infoWindow;
                         infoWindow.open(map, marker);
-                    })
+                    });
 
                     const inputContainer = document.createElement("div");
                     inputContainer.class = "input-container";
@@ -287,33 +68,50 @@ function initMap(){
 
                     placeInput.type = "checkbox";
                     placeInput.checked = true;
-                    // might change what the id should be set to... check for a place id in results
-                    placeInput.id = place.name;
-                    labelPlaceInput.for = place.name;
+                    placeInput.id = place.place_id;
+                    placeInput.classList.add("selected");
+                    placeInput.name = place.place_id;
+                    placeInput.value = place.name;
+                    labelPlaceInput.htmlFor = place.place_id;
                     labelPlaceInput.textContent = place.name;
-                    placeInput.class = "selected";
 
                     inputContainer.appendChild(placeInput);
                     inputContainer.appendChild(labelPlaceInput);
 
-                    placeInput.addEventListener("mouseover", () => {
-                        marker.icon.scaledSize = new google.maps.Size(35, 35);
-                        marker.icon.anchor = new google.maps.Point(0, 35);
+                    inputContainer.addEventListener("mouseover", () => {
+                        bigSize = new google.maps.Size(35, 35);
+                        bigAnchor = new google.maps.Point(0, 35);
+                        marker.setIcon({
+                            ...marker.icon,
+                            scaledSize: bigSize,
+                            anchor: bigAnchor,
+                        });
                     });
 
-                    placeInput.addEventListener("mouseout", () => {
-                        marker.icon.scaledSize = new google.maps.Size(25, 25);
-                        marker.icon.anchor = new google.maps.Point(0, 25);
+                    inputContainer.addEventListener("mouseout", () => {
+                        smallSize = new google.maps.Size(25, 25);
+                        smallAnchor = new google.maps.Point(0, 25);
+                        marker.setIcon({
+                            ...marker.icon,
+                            scaledSize: smallSize,
+                            anchor: smallAnchor,
+                        });
                     });
 
                     placeInput.addEventListener("click", () => {
-                        if (placeInput.class == "selected") {
-                            marker.icon.url = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-                            placeInput.class = "unselected";
+                        if (placeInput.classList.contains("selected")) {
+                            marker.setIcon({...marker.icon, 
+                                url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+                            });
+                            placeInput.classList.add("unselected");
+                            placeInput.classList.remove("selected");
                         }
-                        else {
-                            marker.icon.url = "/static/checkPin.png";
-                            placeInput.class = "selected";
+                        else if (placeInput.classList.contains("unselected")) {
+                            marker.setIcon({...marker.icon, 
+                                url: "/static/checkPin.png",
+                            });
+                            placeInput.classList.add("selected");
+                            placeInput.classList.remove("unselected");
                         }
                     });
                 }
@@ -321,7 +119,7 @@ function initMap(){
 
             const submitButton = document.createElement("input");
             submitButton.type = "submit";
-            submitButton.textContent = "Submit Selected Restaurants";
+            submitButton.value = "Submit Selected Restaurants";
             listOfPlaces.appendChild(submitButton);
 
         }
